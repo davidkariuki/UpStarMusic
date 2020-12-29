7a0303b2f82b28a947b5b08ae32437c5d3b0f0f6
David Kariukí <davidkariuki@users.noreply.github.com>
2020-12-29 17:10:53 +0000

    WIP

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,12 @@
-// Todo: create Album Schema
+const mongoose = require("mongoose")
+
+const AlbumSchema = mongoose.Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number,
+})
+
+module.exports = AlbumSchema
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,17 @@
-// Todo: Create Artist Model
+const mongoose = require("mongoose")
+import AlbumSchema from "./album"
+
+const ArtistSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema],
+})
+
+module.exports = mongoose.model("artist", ArtistSchema)
