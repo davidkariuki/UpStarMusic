a68b4d5d553ec3afd7cd4d827323e960decc6021
David Kariukí <davidkariuki@users.noreply.github.com>
2020-12-30 17:32:24 +0000

    seed 15k, update count and use placeholder images

--- src/seeds.js
+++ src/seeds.js
@@ -1,46 +1,45 @@
-import _ from 'lodash';
-import faker from 'faker';
-import { Db, Server } from 'mongodb';
-import { GENRES } from './constants';
+import _ from "lodash"
+import faker from "faker"
+import { Db, Server } from "mongodb"
+import { GENRES } from "./constants"
 
-const MINIMUM_ARTISTS = 2;
-const ARTISTS_TO_ADD = 15;
+const MINIMUM_ARTISTS = 16
+const ARTISTS_TO_ADD = 15000
 
-let artistsCollection;
-const db = new Db('upstar_music', new Server('localhost', 27017));
+let artistsCollection
+const db = new Db("upstar_music", new Server("localhost", 27017))
 db.open()
   .then(() => {
-    artistsCollection = db.collection('artists');
-    return artistsCollection.count({});
+    artistsCollection = db.collection("artists")
+    return artistsCollection.count({})
   })
-  .then(count => {
+  .then((count) => {
     if (count < MINIMUM_ARTISTS) {
-      const artists = _.times(ARTISTS_TO_ADD, () => createArtist());
+      const artists = _.times(ARTISTS_TO_ADD, () => createArtist())
 
-      artistsCollection.insertMany(artists);
+      artistsCollection.insertMany(artists)
     }
   })
-  .catch(e => console.log(e));
-
+  .catch((e) => console.log(e))
 
 function createArtist() {
   return {
     name: faker.name.findName(),
     age: randomBetween(15, 45),
     yearsActive: randomBetween(0, 15),
-    image: faker.image.avatar(),
+    image: "https://via.placeholder.com/300",
     genre: getGenre(),
     website: faker.internet.url(),
     netWorth: randomBetween(0, 5000000),
     labelName: faker.company.companyName(),
     retired: faker.random.boolean(),
-    albums: getAlbums()
-  };
+    albums: getAlbums(),
+  }
 }
 
 function getAlbums() {
   return _.times(randomBetween(0, 5), () => {
-    const copiesSold = randomBetween(0, 1000000);
+    const copiesSold = randomBetween(0, 1000000)
 
     return {
       title: _.capitalize(faker.random.words()),
@@ -48,26 +47,26 @@ function getAlbums() {
       copiesSold,
       numberTracks: randomBetween(1, 20),
       image: getAlbumImage(),
-      revenue: copiesSold * 12.99
-    };
-  });
+      revenue: copiesSold * 12.99,
+    }
+  })
 }
 
 function getAlbumImage() {
-  const types = _.keys(faker.image);
-  const method = randomEntry(types);
+  const types = _.keys(faker.image)
+  const method = randomEntry(types)
 
-  return faker.image[method]();
+  return faker.image[method]()
 }
 
 function getGenre() {
-  return randomEntry(GENRES);
+  return randomEntry(GENRES)
 }
 
 function randomEntry(array) {
-  return array[~~(Math.random() * array.length)];
+  return array[~~(Math.random() * array.length)]
 }
 
 function randomBetween(min, max) {
-  return ~~(Math.random() * (max-min)) + min;
+  return ~~(Math.random() * (max - min)) + min
 }
